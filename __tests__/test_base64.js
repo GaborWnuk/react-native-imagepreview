a965f0abce682b9a6cb4bb6484ffe0fce324e851
Gabor Wnuk <gabor.wnuk@grupawp.pl>
2017-03-29 17:25:43 +0200

    Added graceful fail for incorrect base64 + tests.

--- __tests__/test_base64.js
+++ __tests__/test_base64.js
@@ -7,7 +7,7 @@ var im = require('imagemagick');
 import B64ImagePreview from '../b64';
 
 describe('base64 to Image', function() {
-  describe('Create proper Image from headless base64', function() {
+  describe('Create proper Image from headless base64', () => {
     var imagePreview = new B64ImagePreview(
       'AD8ApbT61atYA43sM88Cq2au2zHyV244z1NVLRAtWSyQKRgIPyqhKhRyh7VpgsTztxVG9x5vHXHNKMhyRV7UU9QNozTuPSr5SbjGwOpp9tLtfbgkN6etV8HPvT4nMciuD905qG7jWhpB9qnksQMkbelUJ3BmcqQwzwfWrzLGmppktyM5J71nSHdIx9SaS0G3cVXx1p9RUuTVKVibCSEGVsdMmkpG/wBY31o7VIy1cuWEMgPJjHPuOKrDk1LJ/wAe0P8AwL+dQigBc0ZpDRQB/9k=',
     );
@@ -22,7 +22,15 @@ describe('base64 to Image', function() {
       );
     });
 
-    it('Should have proper dimensions', function(done) {
+    it('Should fail gracefully if b64 is invalid', () => {
+      var ip = new B64ImagePreview('Dk1LJ/wAe0P8AwL+dQigBc0ZpDRQB/9k=');
+
+      expect(ip.base64Binary).to.be.null;
+      expect(ip.base64String).to.be.null;
+      expect(ip.imageSource).to.be.null;
+    });
+
+    it('Should have proper dimensions', done => {
       im.identify(
         { data: imagePreview.base64Binary },
         function(error, features) {
@@ -34,7 +42,7 @@ describe('base64 to Image', function() {
       );
     });
 
-    it('Should be of proper type (JPEG)', function(done) {
+    it('Should be of proper type (JPEG)', done => {
       im.identify(
         { data: imagePreview.base64Binary },
         function(error, features) {
@@ -44,7 +52,7 @@ describe('base64 to Image', function() {
       );
     });
 
-    it('Should create proper image source (for HTML base64)', function(done) {
+    it('Should create proper image source (for HTML base64)', done => {
       let b64Prefix = 'data:image/jpg;base64';
       var imageSource = imagePreview.imageSource;
 
--- b64.js
+++ b64.js
@@ -13,24 +13,46 @@ if (!global.btoa) {
 }
 
 export default class B64ImagePreview {
-  b64Header = '/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDADUlKC8oITUvKy88OTU/UIVXUElJUKN1e2GFwarLyL6qurfV8P//1eL/5re6////////////zv//////////////2wBDATk8PFBGUJ1XV53/3Lrc////////////////////////////////////////////////////////////////////wAARCAAqACoDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxE=';
+  static b64Header = '/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDADUlKC8oITUvKy88OTU/UIVXUE' +
+    'lJUKN1e2GFwarLyL6qurfV8P//1eL/5re6////////////zv/////////' +
+    '/////2wBDATk8PFBGUJ1XV53/3Lrc////////////////////////////' +
+    '////////////////////////////////////////wAARCAAqACoDASIAA' +
+    'hEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtR' +
+    'AAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0K' +
+    'xwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVW' +
+    'V1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp' +
+    '6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8v' +
+    'P09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8Q' +
+    'AtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKR' +
+    'obHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU' +
+    '1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoq' +
+    'OkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6On' +
+    'q8vP09fb3+Pn6/9oADAMBAAIRAxE=';
 
-  constructor(b64Body) {
-    this.b64HeaderBinary = atob(this.b64Header);
-    this.b64BodyBinary = atob(b64Body);
+  constructor(b64Body: string) {
+    this.b64HeaderBinary = atob(B64ImagePreview.b64Header);
+
+    try {
+      this.b64BodyBinary = atob(b64Body);
+    } catch (exception) {
+      this.b64BodyBinary = false;
+    }
+
     this.b64Binary = this.b64HeaderBinary + this.b64BodyBinary;
     this.b64String = btoa(this.b64Binary);
   }
 
-  get base64Binary() {
-    return this.b64Binary;
+  get base64Binary(): string {
+    return this.b64BodyBinary ? this.b64Binary : null;
   }
 
-  get base64String() {
-    return this.b64String;
+  get base64String(): string {
+    return this.b64BodyBinary ? this.b64String : null;
   }
 
-  get imageSource() {
-    return 'data:image/jpg;base64,' + this.b64String;
+  get imageSource(): string {
+    return this.b64BodyBinary
+      ? 'data:image/jpg;base64,' + this.b64String
+      : null;
   }
 }
